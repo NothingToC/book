15ac80acbb832071c821c433ebaa2a063cb17305
Anton Knyazev <knyazevmac@icloud.com>
2024-02-20 20:57:14 +0300

    page 224

--- task/hello/firstapp/views.py
+++ task/hello/firstapp/views.py
@@ -1,9 +1,16 @@
 from django.http import *
 from django.shortcuts import render
+from .forms import UserForm
 
-def index(request):   
-    cat = ["Ноутбуки", "Принтеры", "Сканеры", "диски", "Шнуры"]
-    return render(request, 'firstapp/index.html', context={'cat':cat})
+def index(request):
+    userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data['name']
+            return HttpResponse(f'<h1>Name entered correct</h1> {name}')
+        
+    return render(request, 'firstapp/index.html', context={"form": userform})
 
 def about(request):
     return HttpResponse('About')
