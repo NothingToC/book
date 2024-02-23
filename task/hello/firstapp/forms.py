bd7f9bcb9f77179c420b55f28172fbbd0aa766d1
Anton Knyazev <knyazevmac@icloud.com>
2024-02-23 12:54:25 +0300

    page 231

--- task/hello/firstapp/forms.py
+++ task/hello/firstapp/forms.py
@@ -2,5 +2,5 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Name")
-    age = forms.IntegerField(label="Age")
+    name = forms.CharField(label="Name", widget=forms.TextInput(attrs={'class': 'myfield'}))
+    age = forms.IntegerField(label="Age", widget=forms.NumberInput(attrs={'class': 'myfield'}))
