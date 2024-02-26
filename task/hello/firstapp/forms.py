4fdb6ed3becb69b8fb7c3aee582a549af4e436c9
NothingToC <147397298+NothingToC@users.noreply.github.com>
2024-02-26 13:17:40 +0300

    Delete task/hello directory

--- task/hello/firstapp/forms.py
+++
@@ -1,6 +0,0 @@
-
-from django import forms
-
-class UserForm(forms.Form):
-    name = forms.CharField(label="Name", widget=forms.TextInput(attrs={'class': 'myfield'}))
-    age = forms.IntegerField(label="Age", widget=forms.NumberInput(attrs={'class': 'myfield'}))
